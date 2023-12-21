c883d9ab714d788d8ab7681edc12a0c8da871962
claudiofsn <craudiofelix@gmail.com>
2023-12-21 09:44:45 -0300

    Home page created

---
+++ app/(store)/search/loading.tsx
@@ -0,0 +1,5 @@
+export default function SearchLoading() {
+    return (
+        <h1>Procurando</h1>
+    )
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+            hostname: "github.com",
+            protocol: "https"
+        }]
+    },
+
+}
 
 module.exports = nextConfig
